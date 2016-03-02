daddee3545dfb4b47f5e4ed6a53d9e3936ce2ec5
totalamd <totalamd@users.noreply.github.com>
2016-03-02 20:14:01 +0300

    Create top scroll bar bookmarklet.js

---
+++ top scroll bar bookmarklet.js
@@ -0,0 +1,20 @@
+const divContainer = document.createElement('div');
+const divBar = document.createElement('div');
+divContainer.appendChild(divBar);
+divContainer.style.position = 'fixed';
+divContainer.style.opacity = '0.5';
+divContainer.style.height = '10px';
+divContainer.style.width = '100%';
+divContainer.style.top = '0';
+divContainer.style.left = '0';
+divContainer.style.backgroundColor = 'blue';
+divContainer.style.zIndex = '2147483647';
+divBar.style.height = '100%';
+divBar.style.width = Math.min(window.scrollY / (document.body.scrollHeight - window.innerHeight), 1) * 100 + '%';
+divBar.style.backgroundColor = 'hotpink';
+document.body.appendChild(divContainer);
+
+window.addEventListener('scroll', function() {
+  divBar.style.width = Math.min(window.scrollY / (document.body.scrollHeight - window.innerHeight), 1) * 100 + '%';
+	divContainer.title = parseFloat(divBar.style.width).toFixed();
+})
