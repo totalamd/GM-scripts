a4ce3fab503f92f97a2a87dd4d0a990cf11a9795
totalamd <totalamd@users.noreply.github.com>
2016-03-17 23:04:50 +0300

    Added files via upload

---
+++ vk.com_remomve_away.php_redirect.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name            vk.com remove /away.php redirect
+// @name:ru         %name%
+// @description:ru  %description%
+// @namespace       github.com/totalamd
+// @match           *://vk.com/*
+// @version         1
+// @downloadURL 
+// @updateURL   
+// @grant           none
+// @noframes
+// ==/UserScript==
+
+// TODO:
+// - [ ] detect page mutations
+
+"use strict";
+
+(function(){
+	let linksList = Array.from(document.querySelectorAll('a[href^="/away.php?to=http"]'));
+	console.info(linksList);
+	const re = /https?\:\/\/vk.com\/away\.php\?to=([^&]*)(?:&post=[\d_-]*)?/;
+	linksList.forEach(function(link){
+		link.href = decodeURIComponent(link.href.match(re)[1]);
+	});
+}())
