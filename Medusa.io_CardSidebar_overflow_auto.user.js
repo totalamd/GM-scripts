4b6725dc97f2998371e87a50fbb65407a766a0d2
totalamd <totalamd@users.noreply.github.com>
2016-03-17 23:14:19 +0300

    Added files via upload

---
+++ Medusa.io_CardSidebar_overflow_auto.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name        Medusa.io CardSidebar overflow auto
+// @namespace   totalamd
+// @match       *://meduza.io/cards/*
+// @version     1
+// @downloadURL 
+// @updateURL   
+// @grant       none
+// ==/UserScript==
+
+"use strict";
+
+(function(){
+  if (window.self === window.top) {
+		if (new Date() > Date.parse('Mar 01 2016')) {
+			alert('Отключи скрипт для Meduza.io и проверь скролл на карточках');
+		} else {
+			document.querySelector('.CardSidebar').style.overflow = 'auto';
+			// alert('card ok!');
+		}
+  }
+}())
