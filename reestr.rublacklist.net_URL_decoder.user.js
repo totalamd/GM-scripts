e29645dfb1c2760d05f2fcbd1360d8b7160a0fd8
totalamd <totalamd@users.noreply.github.com>
2016-03-19 22:56:10 +0300

    Update reestr.rublacklist.net_URL_decoder.user.js

--- reestr.rublacklist.net_URL_decoder.user.js
+++ reestr.rublacklist.net_URL_decoder.user.js
@@ -6,17 +6,17 @@
 // @namespace       github.com/totalamd
 // @match           *://*.rublacklist.net/*
 // @exclude         
-// @version         1
-// @downloadURL 
-// @updateURL   
+// @version         1.0.0.1
+// @downloadURL     https://github.com/totalamd/GM-scripts/raw/master/reestr.rublacklist.net_URL_decoder.user.js
+// @updateURL       https://github.com/totalamd/GM-scripts/raw/master/reestr.rublacklist.net_URL_decoder.user.js
 // @grant           none
 // @noframes
 // ==/UserScript==
 
 "use strict";
+const l = function(){}, i = function(){};
 
 (function(){
-	let l = function(){}, i = function(){};
 	const l = console.log.bind(console, `${GM_info.script.name} debug:`), i = console.info.bind(console, `${GM_info.script.name} debug:`);
 
 	const linksList = Array.from(document.querySelectorAll('a[href^="/rec/"]'));
