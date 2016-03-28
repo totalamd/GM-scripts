65f326ed6d000ea5047d4098636fe728bfe5475a
totalamd <totalamd@users.noreply.github.com>
2016-03-28 17:38:24 +0300

    add download and update URLs

--- Rutracker_magnet-link_for_closed_seeds.user.js
+++ Rutracker_magnet-link_for_closed_seeds.user.js
@@ -6,8 +6,8 @@
 // @namespace				github.com/totalamd
 // @match						*://rutracker.org/forum/viewtopic.php?t=*
 // @version					1
-// @downloadURL			
-// @updateURL				
+// @downloadURL			https://github.com/totalamd/GM-scripts/raw/master/Rutracker_magnet-link_for_closed_seeds.user.js
+// @updateURL				https://github.com/totalamd/GM-scripts/raw/master/Rutracker_magnet-link_for_closed_seeds.user.js
 // @grant						none
 // @noframes				
 // ==/UserScript==
@@ -16,8 +16,8 @@
 
 (function(){
 	if (!document.querySelector('a[href^="magnet:?"]')) {
-		const link = document.querySelector('span#tor-hash');
-		const hash = link.innerHTML;
-		link.outerHTML = `<a href="magnet:?xt=urn:btih:${hash}&tr=http://bt.rutracker.cc/ann?magnet" class="med magnet-link-16" title="Скачать по magnet-ссылке"><img src="http://static.rutracker.org/templates/v1/images/icon_magnet_16_1.png" alt="magnet"><span id="tor-hash">${hash}</span></a>`;
+		const span = document.querySelector('span#tor-hash');
+		const hash = span.innerHTML;
+		span.outerHTML = `<a href="magnet:?xt=urn:btih:${hash}&tr=http://bt.rutracker.cc/ann?magnet" class="med magnet-link-16" title="Скачать по magnet-ссылке"><img src="http://static.rutracker.org/templates/v1/images/icon_magnet_16_1.png" alt="magnet"><span id="tor-hash">${hash}</span></a>`;
 	}
 }())
