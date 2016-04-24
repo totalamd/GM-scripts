54cb79432df314ef319c1a2e63196a4b6b5ec8fc
totalamd <totalamd@users.noreply.github.com>
2016-04-24 14:20:52 +0300

    initial release

---
+++ aboutreader_p_text-align_justify.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        about:reader text columns justification
+// @namespace   totalamd
+// @include     /^about:reader/
+// @version     1
+// @downloadURL 
+// @updateURL   
+// @noframes
+// ==/UserScript==
+
+"use strict";
+
+(function(){
+	GM_addStyle('#article-text {text-align: justify;}');
+}())
