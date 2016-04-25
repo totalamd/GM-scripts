900172f645d5c969f4cda4bd8107f6e008fcf484
totalamd <totalamd@users.noreply.github.com>
2016-04-25 16:23:51 +0300

    initial release

---
+++ allow_fullscreen_on_every_embed_youtube.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name        youtube: allow fullscreen on every embed
+// @namespace   totalamd github
+// @match       *://*/*
+// @version     1.0.0.0
+// @downloadURL https://github.com/totalamd/GM-scripts/raw/master/allow_fullscreen_on_every_embed_youtube.user.js
+// @updateURL   https://github.com/totalamd/GM-scripts/raw/master/allow_fullscreen_on_every_embed_youtube.user.js
+// @noframes
+// ==/UserScript==
+
+"use strict";
+const l = function(){}, i = function(){};
+
+(function(){
+	// const l = console.log.bind(console, `${GM_info.script.name} debug:`), i = console.info.bind(console, `${GM_info.script.name} debug:`);
+
+	Array.from(document.querySelectorAll('iframe')).forEach( (iframe) => {
+		if (iframe.src.match(/^https?:\/\/(?:www\.)?youtube\.com\/embed\//)) {
+			iframe.allowFullscreen = true;
+		}
+	});
+
+	const target = document.body;
+	const config = {
+		childList : true,
+		subtree: true
+	};
+	const observer = new MutationObserver( (mutations) => {
+		mutations.forEach( (mutation) => {
+			Array.from(mutation.addedNodes).forEach( (node) => {
+				if (node.tagName === "IFRAME" && node.src.match(/^https?:\/\/(?:www\.)?youtube\.com\/embed\//)) {
+					node.allowFullscreen = true;
+				}
+			});
+		});
+	});
+	
+	observer.observe(target, config);
+})();
